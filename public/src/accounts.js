3635ec3c38033afa0e25ff4a1f758b1e2540170e
Sean Bunch <databaws@gmail.com>
2022-05-30 22:27:41 -0400

    code is cleaned of extra console.logs and extra comments

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -5,24 +5,17 @@ function findAccountById(accounts, id) {
 function sortAccountsByLastName(accounts) {
   return accounts.sort((a, b) => (a.name.last > b.name.last) ? 1 : -1);
 };
-// console.log(sortAccountsByLastName(accounts));
-// console.log(books[0].borrows[0].id);
-// console.log(accounts[0].id);
-// const account = accounts[10];
 function getTotalNumberOfBorrows(account, books) {
   let count = 0;
   for (let i = 0; i < books.length; i++) {
     for (let j = 0; j < books[i].borrows.length; j++) {
-      // console.log(books[i].borrows[j].id);
       if(account.id === books[i].borrows[j].id) {
         count += 1;
       };
     };
   };
-  // console.log(count);
   return count;
 };
-// console.log(getTotalNumberOfBorrows(account, books));
 
 function getBooksPossessedByAccount(account, books, authors) { }
 
