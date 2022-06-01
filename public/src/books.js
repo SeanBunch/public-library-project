7fb7b511fcf5cda7eeef9676f2c9887749936085
Sean Bunch <databaws@gmail.com>
2022-06-01 16:30:57 -0400

    extra console.logs and extra comments removed

--- public/src/books.js
+++ public/src/books.js
@@ -1,25 +1,12 @@
-// const id = 2;
 function findAuthorById(authors, id) {
-  // let result = authors.find((author) => author.id === id)
   let result = null;
     authors.reduce((foundAuthor, author) => {
-      // console.log(foundAuthor)
-      // console.log(author[id]);
-      // console.log(id);
-      // console.log(author.id);
       if (author.id === id) {
         result = author;
       };
-      // console.log(author);
-      // return foundAuthor;
     }, null);
-  // console.log(result);
   return result;
 };
-// console.log(`author object:`, authors[id], `id number:`, id);
-// console.log(findAuthorById(authors, id));
-
-
 function findBookById(books, id) { }
 
 function partitionBooksByBorrowedStatus(books) { }
