628ec29ea2c5e2badb1d383573c2fcefab5a0c9a
Sean Bunch <databaws@gmail.com>
2022-05-31 23:31:44 -0400

    code cleaned of extra console.logs and extra comments

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -9,7 +9,6 @@ function getTotalNumberOfBorrows(account, books) {
   let count = 0;
   for (let i = 0; i < books.length; i++) {
     for (let j = 0; j < books[i].borrows.length; j++) {
-      // console.log(books[i].borrows[j].id);
       if (account.id === books[i].borrows[j].id) {
         count += 1;
       };
@@ -17,22 +16,12 @@ function getTotalNumberOfBorrows(account, books) {
   };
   return count;
 };
-
-// const account = accounts[2];
-// console.log(account);
 function getBooksPossessedByAccount(account, books, authors) {
   let result = [];
   for (let i = 0; i < books.length; i++) {
     for (let j = 0; j < books[i].borrows.length; j++) {
       if (account.id === books[i].borrows[j].id && books[i].borrows[j].returned === false) {
-        // console.log(books[i].borrows[j].returned)
-        // console.log("book is:", books[i]);
         result.push(books[i]);
-        // console.log(result);
-
-        // console.log(authors.id[1])
-        // console.log("account is:", account);
-        // console.log(books[i].borrows[j].id);
       };
     };
   };
@@ -40,16 +29,11 @@ function getBooksPossessedByAccount(account, books, authors) {
     for (let j = 0; j < authors.length; j++) {
       if (result[i].authorId === authors[j].id) {
         result[i]["author"] = authors[j]
-
       };
     };
   };
   return result;
-  // console.log(result[0].authorId);
-  // console.log(result);
-  // console.log(account, books, authors);
 };
-// console.log(getBooksPossessedByAccount(account, books, authors));
 
 module.exports = {
   findAccountById,
