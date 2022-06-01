a9882bc75a9888d0bc40f414b83fc10aaeec1f67
Sean Bunch <databaws@gmail.com>
2022-05-31 23:26:45 -0400

    last accounts.js function solved with forLoops and ifs

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -35,7 +35,7 @@ function getTotalNumberOfBorrows(account, books) {
   for (let i = 0; i < books.length; i++) {
     for (let j = 0; j < books[i].borrows.length; j++) {
       // console.log(books[i].borrows[j].id);
-      if(account.id === books[i].borrows[j].id) {
+      if (account.id === books[i].borrows[j].id) {
         count += 1;
       };
     };
@@ -45,7 +45,38 @@ function getTotalNumberOfBorrows(account, books) {
 };
 // console.log(getTotalNumberOfBorrows(account, books));
 
-function getBooksPossessedByAccount(account, books, authors) { }
+// const account = accounts[2];
+// console.log(account);
+function getBooksPossessedByAccount(account, books, authors) {
+  let result = [];
+  for (let i = 0; i < books.length; i++) {
+    for (let j = 0; j < books[i].borrows.length; j++) {
+      if (account.id === books[i].borrows[j].id && books[i].borrows[j].returned === false) {
+        // console.log(books[i].borrows[j].returned)
+        // console.log("book is:", books[i]);
+        result.push(books[i]);
+        // console.log(result);
+
+        // console.log(authors.id[1])
+        // console.log("account is:", account);
+        // console.log(books[i].borrows[j].id);
+      };
+    };
+  };
+  for (let i = 0; i < result.length; i++) {
+    for (let j = 0; j < authors.length; j++) {
+      if (result[i].authorId === authors[j].id) {
+        result[i]["author"] = authors[j]
+
+      };
+    };
+  };
+  return result;
+  // console.log(result[0].authorId);
+  // console.log(result);
+  // console.log(account, books, authors);
+};
+// console.log(getBooksPossessedByAccount(account, books, authors));
 
 module.exports = {
   findAccountById,
