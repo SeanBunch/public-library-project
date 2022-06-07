01c88820b0bb60ad47cf44718b0be3746d239213
Sean Bunch <databaws@gmail.com>
2022-06-07 14:19:17 -0400

    3rd time to fix merge conflicts

--- public/src/home.js
+++ public/src/home.js
@@ -19,13 +19,42 @@ function getBooksBorrowedCount(books) {
   };
   return count;
 };
-
-function getMostCommonGenres(books) { }
-
-
-function getMostPopularBooks(books) { }
-
-function getMostPopularAuthors(books, authors) { }
+function getMostCommonGenres(books) {
+  let bookGenres = {};
+  books.forEach((book) => {
+    if (bookGenres[book.genre]) {
+      bookGenres[book.genre]++;
+    } else {
+      bookGenres[book.genre] = 1;
+    };
+  });
+  return Object.entries(bookGenres).map(([name, count]) => {
+    return { name, count };
+  }).sort((a, b) => (b.count - a.count)).slice(0, 5);
+};
+function getMostPopularBooks(books) {
+  let listOfCount = {};
+  books.forEach((book) => {
+    listOfCount[book.title] = book.borrows.length;
+  });
+  return Object.entries(listOfCount).map(([name, count]) => {
+    return { name, count };
+  }).sort((a, b) => (b.count - a.count)).slice(0, 5);
+};
+function getMostPopularAuthors(books, authors) {
+  let mostPopularAuthors = [];
+  for (let i = 0; i < authors.length; i++) {
+    const authorId = authors[i].id;
+    let popularAuthor = { name: `${authors[i].name.first} ${authors[i].name.last}`, count: 0 };
+    for (let j = 0; j < books.length; j++) {
+      if (authorId === books[j].authorId) {
+        popularAuthor.count += books[j].borrows.length;
+        mostPopularAuthors.push(popularAuthor);
+      };
+    };
+  };
+  return mostPopularAuthors.sort((a, b) => (b.count - a.count)).slice(0, 5);
+};
 
 module.exports = {
   getTotalBooksCount,
