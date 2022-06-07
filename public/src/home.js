bd93c73419098eed3f8dd6e9b64c33af2335e5de
Sean Bunch <databaws@gmail.com>
2022-06-06 20:11:36 -0400

    2 more functionc complete

--- public/src/home.js
+++ public/src/home.js
@@ -34,17 +34,68 @@ function getBooksBorrowedCount(books) {
   return count;
 };
 // console.log(getBooksBorrowedCount(books));
+// count[book.genre] = (count[book.genre] || 0) + 1;
 
+// function listGenre(books) {
+//   let list = [];
+//   const countObj = books.reduce((count, book) => {
+//     count[book.genre] = (count[book.genre] || 0) + 1;
+//     list.push({ "name": book.genre, "count": (count[book.genre] || 0) + 1 })
+//     //  {"name": book.genre}, {"count": (count[book.genre] || 0) + 1 };
+//     // console.log( {"name": book.genre, "count": (count[book.genre] || 0) + 1 })
+//     // console.log(list);
+//     // console.log(count)
+//     return count;
+//   }, []);
+//   return countObj;
+//   // return list
+// }
+// console.log(listGenre(books));
 
 function getMostCommonGenres(books) {
-
-
+  let bookGenres = {};
+  books.forEach((book) => {
+    if (bookGenres[book.genre]) {
+      bookGenres[book.genre]++;
+    } else {
+      bookGenres[book.genre] = 1;
+    }
+  });
+  // console.log(bookGenres);
+  return Object.entries(bookGenres).map(([name, count]) => {
+    return { name, count };
+  }).sort((a, b) => (b.count - a.count)).slice(0, 5);
 };
+
 // console.log(getMostCommonGenres(books));
 
 
 
-function getMostPopularBooks(books) { }
+// function countBooks(books) {
+//   let listOfCount = {};
+//   books.forEach((book) => {
+//     listOfCount["borrowed"] = book.borrows.length;
+//     // console.log(book.borrows.length);
+    
+//     console.log(listOfCount);
+//   });
+//   // console.log(listOfCount);
+// };
+// console.log(countBooks(books));
+
+function getMostPopularBooks(books) {
+  let listOfCount = {};
+  books.forEach((book) => {
+    listOfCount[book.title] = book.borrows.length;
+    // console.log(book.borrows.length);
+    // console.log(listOfCount);
+  })
+  // console.log(listOfCount);
+  return Object.entries(listOfCount).map(([name, count]) => {
+    return { name, count };
+  }).sort((a, b) => (b.count - a.count)).slice(0, 5);
+};
+// console.log(getMostPopularBooks(books));
 
 function getMostPopularAuthors(books, authors) { }
 
